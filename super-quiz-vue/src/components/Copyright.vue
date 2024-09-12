519dc63cb7726fa304da71ade292867e72873fdd
Cynthia <cynthiaareasjardim@gmail.com>
2024-09-12 20:40:33 -0300

    Adiciona componente de copyright

---
+++ super-quiz-vue/src/components/Copyright.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+      <p>&copy; 2024 Cynthia Jardim.</p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+  }
+  </script>
+  
+  <style scoped>
+  footer {
+    text-align: center;
+    margin-top: 60px;
+    padding: 10px;
+  }
+  </style>
